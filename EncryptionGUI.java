bce3ea2835ec0d6f00a8c4cbe4128c87433833ba
Tony Tipton <tttipton@asu.edu>
2019-09-22 19:55:44 -0700

    Added Encryption, Password Breaking, and Phrase to Password GUI sections
    Coded the front end of the three sections into the main GUI to be used for password analysis.

--- EncryptionGUI.java
+++ EncryptionGUI.java
@@ -6,6 +6,7 @@ import javafx.geometry.Insets;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -13,29 +14,16 @@ import javafx.scene.layout.VBox;
 import java.lang.String;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.*;
 
 public class EncryptionGUI extends Application {
 
-    private Stage defaultWindow;
-    private Button generatePassword;
     private Label currentPasswordLabel;
-    private Label passwordGenerationCharacters;
-    private Label passwordStrengthLabel;
-    private Label passwordHashedStrengthLabel;
-    private Label currentPasswordHash;
     private String password = "";
-    private String passwordHashed = "test test";
     private String passwordType;
-    private Label passwordSize;
     private TextField desiredSize;
-    private HBox passwordBox;
-    private HBox  passwordStrengthBox;
-    private HBox passwordHashedStrengthBox;
-    private HBox passwordTypesRow;
-    private VBox passwordTypesColumn1;
-    private VBox passwordTypesColumn2;
     private ImageView passwordStrengthImage;
-    private ImageView passwordHashedStrengthImage;
     private static String stringSize;
     private static int size;
     private CheckBox userDefined;
@@ -43,7 +31,6 @@ public class EncryptionGUI extends Application {
     private CheckBox upperCase;
     private CheckBox numbers;
     private CheckBox specialCharacters;
-
 
 
     public static void main(String[] args) {
@@ -54,21 +41,15 @@ public class EncryptionGUI extends Application {
     public void start(Stage primaryStage)
     {
         // Application Window Section
-        defaultWindow = primaryStage;
-        defaultWindow.setTitle("Encryption GUI");
-        generatePassword = new Button ("Generate Password");
-        currentPasswordLabel = new Label ("Current Password:  " + password);
-        passwordStrengthLabel = new Label("Password Strength:   ");
-        currentPasswordHash = new Label("Password Hashed:  " + passwordHashed);
-        passwordHashedStrengthLabel = new Label("Hashed Password Strength:   ");
-        //passwordStengthImage = new Image("yeet.png");
-        //currentPassword = new Label (password);
+        primaryStage.setTitle("Encryption GUI");
 
-        // CheckBox Section
-        passwordGenerationCharacters = new Label("Select Password Characters");
-        passwordTypesRow = new HBox();
-        passwordTypesColumn1 = new VBox();
-        passwordTypesColumn2 = new VBox();
+        // Generate a Password Section
+        Text passwordGenerationCharactersText = new Text ("Password Character Selection");
+        passwordGenerationCharactersText.setFont(Font.font("Segoe UI",FontWeight.EXTRA_BOLD, 14));
+        Label passwordGenerationCharacters = new Label();
+        HBox passwordTypesRow = new HBox();
+        VBox passwordTypesColumn1 = new VBox();
+        VBox passwordTypesColumn2 = new VBox();
         lowerCase = new CheckBox("Lower Case");
         upperCase = new CheckBox("Upper Case");
         numbers = new CheckBox("Numbers");
@@ -76,11 +57,65 @@ public class EncryptionGUI extends Application {
         userDefined = new CheckBox("Custom Password");
         passwordTypesColumn1.getChildren().addAll(lowerCase,upperCase,userDefined);
         passwordTypesColumn2.getChildren().addAll(numbers,specialCharacters);
-        passwordTypesRow.getChildren().addAll(passwordTypesColumn1,passwordTypesColumn2);
+        passwordTypesRow.getChildren().addAll(passwordTypesColumn1, passwordTypesColumn2);
         passwordTypesRow.setSpacing(20);
         passwordTypesColumn1.setSpacing(5);
         passwordTypesColumn2.setSpacing(5);
+        Button generatePassword = new Button("Generate Password");
+        currentPasswordLabel = new Label ("Current Password:  " + password);
+        Label passwordStrengthLabel = new Label("Password Strength:   ");
 
+        // Hash Function Section
+        Text encryptionSectionText = new Text ("Encryption Type Selection");
+        encryptionSectionText.setFont(Font.font("Segoe UI",FontWeight.EXTRA_BOLD, 14));
+        HBox encryptionSectionRow = new HBox();
+        VBox encryptionSectionColumn1 = new VBox();
+        VBox encryptionSectionColumn2 = new VBox();
+        CheckBox encryption1 = new CheckBox("encryption1");
+        CheckBox encryption2 = new CheckBox("encryption2");
+        CheckBox encryption3 = new CheckBox("encryption3");
+        CheckBox encryption4 = new CheckBox("encryption4");
+        CheckBox encryption5 = new CheckBox("encryption5");
+        CheckBox encryption6 = new CheckBox("encryption6");
+        encryptionSectionColumn1.getChildren().addAll(encryption1, encryption2, encryption3);
+        encryptionSectionColumn2.getChildren().addAll(encryption4, encryption5, encryption6);
+        encryptionSectionRow.getChildren().addAll(encryptionSectionColumn1, encryptionSectionColumn2);
+        encryptionSectionRow.setSpacing(20);
+        encryptionSectionColumn1.setSpacing(5);
+        encryptionSectionColumn2.setSpacing(5);
+        Button generateHash = new Button("Generate Encrption");
+        String passwordHashed = "test test";
+        Label currentPasswordHash = new Label("Password Encrypted:  " + passwordHashed);
+        Label passwordHashedStrengthLabel = new Label("Encrypted Password Strength:   ");
+
+        // Password Braking Section
+        Text pwBreakSectionText = new Text ("Select a Password Breaking Method");
+        pwBreakSectionText.setFont(Font.font("Segoe UI",FontWeight.EXTRA_BOLD, 14));
+        HBox pwBreakSectionRow = new HBox();
+        VBox pwBreakSectionColumn1 = new VBox();
+        VBox pwBreakSectionColumn2 = new VBox();
+        CheckBox pwBreak1 = new CheckBox("Option 1");
+        CheckBox pwBreak2 = new CheckBox("Option 2");
+        pwBreakSectionColumn1.getChildren().addAll(pwBreak1);
+        pwBreakSectionColumn2.getChildren().addAll(pwBreak2);
+        pwBreakSectionRow.getChildren().addAll(pwBreakSectionColumn1, pwBreakSectionColumn2);
+        pwBreakSectionRow.setSpacing(20);
+        pwBreakSectionColumn1.setSpacing(5);
+        pwBreakSectionColumn2.setSpacing(5);
+        Button tryToBreak = new Button("Break");
+        Label tryToBreakPW = new Label("Current Password:   ");
+        Label tryToBreakHash = new Label("Hashed Password:   ");
+
+        // Phrase to Password Section
+        Text P2PSectionText = new Text ("Phrase to Password Tool");
+        P2PSectionText.setFont(Font.font("Segoe UI",FontWeight.EXTRA_BOLD, 14));
+        Label P2PPhrase = new Label("Phrase:   ");
+        Label P2PPassword = new Label("Created Password:   ");
+        Button generate = new Button("Generate");
+        VBox P2PSectionColumn = new VBox();
+        P2PSectionColumn.getChildren().addAll(P2PPhrase, P2PPassword,generate);
+        P2PSectionColumn.setSpacing(10);
+        // Generate Password Process
         generatePassword.setOnAction(e -> {
             try {
                 stringSize = desiredSize.getText();
@@ -122,40 +157,42 @@ public class EncryptionGUI extends Application {
                         EncryptionGUI.message("Please select another combination of password characters");
 
                     currentPasswordLabel.setText("Current Password:  " + password);
-                    passwordStrengthImage.setImage(passwordGenerator.determinePasswordStrength());
-
+                    passwordStrengthImage.setImage(PasswordStrength.determinePasswordStrengthImage(password));
                 }
-
             catch(NumberFormatException i)
             {
                 EncryptionGUI.message("Please Enter a Numeric Value for Password");
             }
         });
-        passwordSize = new Label("Password Size:  ");
+
+        Label passwordSize = new Label("Password Size:  ");
         desiredSize = new TextField();
-        passwordBox = new HBox();
-        //passwordBox.setPadding(new Insets(10,10,10,10));
+        HBox passwordBox = new HBox();
         passwordBox.getChildren().addAll(passwordSize,desiredSize);
 
         passwordStrengthImage = new ImageView();
         Image passwordStrengthBar = new Image(EncryptionGUI.class.getResourceAsStream("White.png"));
         passwordStrengthImage.setImage(passwordStrengthBar);
-        passwordStrengthBox = new HBox();
+        HBox passwordStrengthBox = new HBox();
         passwordStrengthBox.getChildren().addAll(passwordStrengthLabel,passwordStrengthImage);
 
-        passwordHashedStrengthImage = new ImageView();
+        ImageView passwordHashedStrengthImage = new ImageView();
         Image currentPasswordHashedStrengthImage = new Image(EncryptionGUI.class.getResourceAsStream("White.png"));
         passwordHashedStrengthImage.setImage(currentPasswordHashedStrengthImage);
-        //passwordHashedStrengthLabel.setText("Hashed Password Strength");
-        passwordHashedStrengthBox = new HBox();
+        HBox passwordHashedStrengthBox = new HBox();
         passwordHashedStrengthBox.getChildren().addAll(passwordHashedStrengthLabel, passwordHashedStrengthImage);
         VBox defaultLayout = new VBox(10);
         defaultLayout.setPadding(new Insets(10,10,10,10));
-        //passwordStrengthLabel.setUnderline(true);
-        defaultLayout.getChildren().addAll(passwordGenerationCharacters,passwordTypesRow,passwordBox,generatePassword,currentPasswordLabel,passwordStrengthBox,currentPasswordHash,passwordHashedStrengthBox);
-        Scene newScene = new Scene(defaultLayout,400,450);
-        defaultWindow.setScene(newScene);
-        defaultWindow.show();
+        defaultLayout.getChildren().addAll(
+                passwordGenerationCharactersText, passwordTypesRow, passwordBox,
+                generatePassword,currentPasswordLabel, passwordStrengthBox,
+                encryptionSectionText, encryptionSectionRow, generateHash, currentPasswordHash,
+                passwordHashedStrengthBox,pwBreakSectionText,pwBreakSectionRow,
+                tryToBreak,tryToBreakPW,tryToBreakHash,P2PSectionText,P2PSectionColumn);
+
+        Scene newScene = new Scene(defaultLayout,400,700);
+        primaryStage.setScene(newScene);
+        primaryStage.show();
     }
 
     public static void message(String message)
@@ -165,7 +202,7 @@ public class EncryptionGUI extends Application {
         VBox MessageGrid = new VBox();
         Button Ok = new Button();
         Label messageToDisplay = new Label();
-        messageToDisplay.setFont(new Font("Times New Roman", 14));
+        messageToDisplay.setFont(new Font("Segoe UI",12));
         messageToDisplay.setText(message);
         messageToDisplay.setTranslateX(85);
         messageToDisplay.setTranslateY(75);
